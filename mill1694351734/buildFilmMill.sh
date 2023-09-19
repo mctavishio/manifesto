5de729b785efd396bfad626500a2588a3427d08d
mctavish <kathy@mctavish.io>
2023-09-19 00:49:09 -0500

    cleaning up Bmill

--- mill1694351734/buildFilmMill.sh
+++ mill1694351734/buildFilmMill.sh
@@ -3,19 +3,22 @@ ts=$(date +"%s")
 echo $ts
 mkdir data/mill$ts
 mkdir data/mill$ts/css
-cp BmillTemplate.js data/mill$ts/Bmill_temp.js
-cp inputTemplate.js data/mill$ts/input.js
+cp Bmill.js data/mill$ts/Bmill_temp.js
 cp frameMill.js data/mill$ts/poemMill.js
 cp filmMill.js data/mill$ts/bookMill.js
 cp css/film/print.css data/mill$ts/css/print.css
+cd css/film
+bash compileCSS.sh
+cd ../..
 cp pdfToFilm.sh data/mill$ts/pdfToFilm.sh
 sed "s/00000000/\"$ts\"/" data/mill$ts/Bmill_temp.js > data/mill$ts/Bmill.js
 rm data/mill$ts/Bmill_temp.js
 node data/mill$ts/Bmill.js
-node poemMill ./data/mill$ts
-node bookMill ./data/mill$ts 
+node frameMill ./data/mill$ts
+node filmMill ./data/mill$ts 
 prince -s data/mill$ts/css/print.css data/mill$ts/print.html -o data/mill$ts/print.pdf
 echo "cd data/mill$ts"
+echo "bash pdfToFilm.sh"
 echo "$(date)" > data/mill$ts/readMe.txt
 echo "directory=data/mill$ts" >> data/mill$ts/readMe.txt
 echo "done"
